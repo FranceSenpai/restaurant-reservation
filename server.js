7a018b722fa4239588185114173f4f2dae802a08
France <francisco12304@hotmail.com>
2025-02-07 20:51:41 -0400

    Add server.js to the correct folder and update package.json

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
+
+// Use the PORT provided by Heroku
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello, Heroku!');
+  res.send('Hello, Heroku!');
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
